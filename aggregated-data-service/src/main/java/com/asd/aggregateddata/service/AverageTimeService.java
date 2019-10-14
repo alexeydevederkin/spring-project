bdcfa5599ff055d7c24adef16cc2421200e7af52
alexeydevederkin <asd.correo@gmail.com>
2019-10-14 15:09:02 +0700

    optimized imports

--- aggregated-data-service/src/main/java/com/asd/aggregateddata/service/AverageTimeService.java
+++ aggregated-data-service/src/main/java/com/asd/aggregateddata/service/AverageTimeService.java
@@ -5,9 +5,8 @@ import com.asd.aggregateddata.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.Period;
-import java.util.List;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Locale;
 
 
--- employee-service/src/main/java/com/asd/employee/repo/EmployeeRepo.java
+++ employee-service/src/main/java/com/asd/employee/repo/EmployeeRepo.java
@@ -1,8 +1,8 @@
 package com.asd.employee.repo;
 
 import com.asd.employee.model.Employee;
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
