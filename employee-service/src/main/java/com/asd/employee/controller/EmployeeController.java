0ad9f32cbd36dbce07229c7abfdd397f461307ff
alexeydevederkin <asd.correo@gmail.com>
2019-10-15 13:01:13 +0700

    added EmployeeService

--- employee-service/src/main/java/com/asd/employee/controller/EmployeeController.java
+++ employee-service/src/main/java/com/asd/employee/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.asd.employee.controller;
 
 import com.asd.employee.model.Employee;
 import com.asd.employee.repo.EmployeeRepo;
+import com.asd.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,52 +12,35 @@ import java.util.List;
 @RequestMapping("employee")
 public class EmployeeController {
 
-    private EmployeeRepo employeeRepo;
+    private EmployeeService employeeService;
 
     @Autowired
-    public EmployeeController(EmployeeRepo employeeRepo) {
-        this.employeeRepo = employeeRepo;
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping
-    public List<Employee> getAllEmployees() {
-        return employeeRepo.findAll();
+    public List<Employee> getAll() {
+        return employeeService.getAllEmployees();
     }
 
     @GetMapping("{id}")
-    public Employee getOneEmployee(@PathVariable("id") Employee employee) {
-        return employee;
+    public Employee getOne(@PathVariable("id") Integer id) {
+        return employeeService.getOneEmployee(id);
     }
 
     @PostMapping
-    public Employee createEmployee(@RequestBody Employee employee) {
-        if (employee.getHireDate().compareTo(employee.getFireDate()) > 0) {
-            return null;
-        }
-        return employeeRepo.save(employee);
+    public Employee create(@RequestBody Employee newEmployee) {
+        return employeeService.createEmployee(newEmployee);
     }
 
     @PutMapping("{id}")
-    public Employee updateEmployee(@PathVariable("id") Integer id, @RequestBody Employee newEmployee) {
-        if (newEmployee.getHireDate().compareTo(newEmployee.getFireDate()) > 0) {
-            return null;
-        }
-
-        return employeeRepo.findById(id).map(employee -> {
-            employee.setName(newEmployee.getName());
-            employee.setDepartment(newEmployee.getDepartment());
-            employee.setPosition(newEmployee.getPosition());
-            employee.setHireDate(newEmployee.getHireDate());
-            employee.setFireDate(newEmployee.getFireDate());
-            return employeeRepo.save(employee);
-        }).orElseGet(() -> {
-            newEmployee.setId(id);
-            return employeeRepo.save(newEmployee);
-        });
+    public Employee update(@PathVariable("id") Integer id, @RequestBody Employee newEmployee) {
+        return employeeService.updateEmployee(id, newEmployee);
     }
 
     @DeleteMapping("{id}")
     public void deleteEmployee(@PathVariable("id") Integer id) {
-        employeeRepo.deleteById(id);
+        employeeService.deleteEmployee(id);
     }
 }
---
+++ employee-service/src/main/java/com/asd/employee/service/EmployeeService.java
@@ -0,0 +1,55 @@
+package com.asd.employee.service;
+
+import com.asd.employee.model.Employee;
+import com.asd.employee.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    private EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee getOneEmployee(Integer id) {
+        return employeeRepo.findById(id).orElse(null);
+    }
+
+    public Employee createEmployee(Employee newEmployee) {
+        if (newEmployee.getHireDate().compareTo(newEmployee.getFireDate()) > 0) {
+            return null;
+        }
+        return employeeRepo.save(newEmployee);
+    }
+
+    public Employee updateEmployee(Integer id, Employee newEmployee) {
+        if (newEmployee.getHireDate().compareTo(newEmployee.getFireDate()) > 0) {
+            return null;
+        }
+
+        return employeeRepo.findById(id).map(employee -> {
+            employee.setName(newEmployee.getName());
+            employee.setDepartment(newEmployee.getDepartment());
+            employee.setPosition(newEmployee.getPosition());
+            employee.setHireDate(newEmployee.getHireDate());
+            employee.setFireDate(newEmployee.getFireDate());
+            return employeeRepo.save(employee);
+        }).orElseGet(() -> {
+            newEmployee.setId(id);
+            return employeeRepo.save(newEmployee);
+        });
+    }
+
+    public void deleteEmployee(Integer id) {
+        employeeRepo.deleteById(id);
+    }
+}
